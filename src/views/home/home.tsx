a66f8ec576b5914b9d911d80d79e78f4f6154d38
Owieczek <112249898+Owieczek@users.noreply.github.com>
2022-12-22 16:28:24 +0100

    appbar

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,5 +1,11 @@
-export const HomeView: React.FC = () => {
-    return <>HomeView</>;
-  };
-
-  export default HomeView;
+import { Outlet } from "react-router-dom";
+import AppBar from "../../components/app-bar";
+const MainView: React.FC = () => {
+  return (
+    <>
+        <Outlet />
+      Footer
+    </>
+  );
+};
+export default MainView;
