c4d2d6a482e140cfe000a5ec2bdbb1f4f6755ee3
Hubert Lewandowski <hubletto@gmail.com>
2022-12-23 00:06:00 +0100

    added news API

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,20 +1,25 @@
 import React,{useEffect,useState} from 'react'
+import axios from 'axios';
 const MainView: React.FC = () => {
-
+  const [gameNews, SetGameNews] = useState("")
   useEffect(()=>{
     const options = {
       method: 'GET',
+      url: 'https://mmo-games.p.rapidapi.com/latestnews',
       headers: {
         'X-RapidAPI-Key': 'd1de6b559emshf05bb40bdef5eeap13bed5jsnf35944932b7f',
         'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com'
       }
     };
     
-    fetch('https://mmo-games.p.rapidapi.com/latestnews', options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err))
-  },[])
+    axios.request(options).then(function (response) {
+      SetGameNews(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }, [])
+
+  console.log(gameNews);
 
   return <>MainView</>
 };
