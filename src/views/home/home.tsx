273e793406aa00c872789203ddb468aa0fc2e09b
Hubert Lewandowski <hubletto@gmail.com>
2022-12-22 23:34:14 +0100

    fff

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,4 +1,21 @@
+import React,{useEffect,useState} from 'react'
 const MainView: React.FC = () => {
+
+  useEffect(()=>{
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd1de6b559emshf05bb40bdef5eeap13bed5jsnf35944932b7f',
+        'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://mmo-games.p.rapidapi.com/latestnews', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err))
+  },[])
+
   return <>MainView</>
 };
 
