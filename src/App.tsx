aba84849b503ce1885062231bb3db75fdc82f561
Hubert Lewandowski <hubletto@gmail.com>
2022-12-23 16:08:36 +0100

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import './App.css';
-import HomeView from './views/home/home';
+import { MainView } from './views/home/home';
 import AppBar from './components/app-bar';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
     <AppBar />
     <Routes>
-      <Route path="/" element={<HomeView/>}/>
+      <Route path="/" element={<MainView/>}/>
       
     </Routes>
     </BrowserRouter>
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import NewsCard from "../../components/news-card";
-const HomeView: React.FC = () => {
- return( <NewsCard />
- );
-};
 
-export default HomeView;
+export const MainView: React.FC = () => {
+    return <NewsCard />
+  };
+  
+  
+  
