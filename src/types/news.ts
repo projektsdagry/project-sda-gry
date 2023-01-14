5ef41f793a0adc907a19d6db229ea552cf654061
Owieczek <112249898+Owieczek@users.noreply.github.com>
2023-01-14 17:08:57 +0100

    feat: services

--- src/types/news.ts
+++ src/types/news.ts
@@ -1,10 +1,9 @@
-export interface New {
-id:number;
-title:string;
-short_description:string;
-thumbnail:string;
-main_image:string;
-article_url:string;
-article_content:string;
-
+export interface News {
+  id: number;
+  title: string;
+  short_description: string;
+  thumbnail: string;
+  main_image: string;
+  article_url: string;
+  article_content: string;
 }
--- src/views/news/news.tsx
+++ src/views/news/news.tsx
@@ -1,32 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import NewsCard from "../../components/news-card/news-card";
-import { New } from "../../types/news";
+import { News } from "../../types/news";
 import Pagination from "@mui/material/Pagination/Pagination";
+import { apiNews } from "../../services/api-news";
 
 export const NewsView: React.FC = () => {
-  const [news, SetNews] = useState([] as New[]);
+  const [news, setNews] = useState<News[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [newsPerPage, setNewsPerPage] = useState(8);
 
   useEffect(() => {
-    const options = {
-      method: "GET",
-      url: "https://mmo-games.p.rapidapi.com/latestnews",
-      headers: {
-        "X-RapidAPI-Key": "d1de6b559emshf05bb40bdef5eeap13bed5jsnf35944932b7f",
-        "X-RapidAPI-Host": "mmo-games.p.rapidapi.com",
-      },
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        SetNews(response?.data || []);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    (async () => {
+      const newsData = await apiNews.getNews();
+      if (newsData) {
+        setNews(newsData);
+      }
+    })();
+    // axios
+    //   .request(options)
+    //   .then(function (response) {
+    //     setNews(response?.data || []);
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
   }, []);
   const indexOfLastNews = currentPage * newsPerPage;
   const indexOfFirstNews = indexOfLastNews - newsPerPage;
@@ -34,7 +32,14 @@ export const NewsView: React.FC = () => {
 
   return (
     <>
-      <div style={{ display: "flex", justifyContent:'center', position: "sticky", top: 0 }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          position: "sticky",
+          top: 0,
+        }}
+      >
         <Pagination
           color="primary"
           count={5}
