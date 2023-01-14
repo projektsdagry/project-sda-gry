becb51f7794b3394e659f3d34502be4100618da2
t1psyy <p.bankowski101@gmail.com>
2023-01-14 19:24:54 +0100

    feat: added rawg API to services

--- src/services/api-rawg.tsx
+++ src/services/api-rawg.tsx
@@ -3,13 +3,12 @@ import { Game } from "../types/game";
 
 const apiGamesDef = () => {
     const getGames = async (page: number): Promise<Game[] | undefined> => {
-    const url = `https://api.rawg.io/api/games?page=${page}&page_size=50&key=47df0f4ea0b14eb3b3beaeba99f4e03d`;
+    
     try {
-      const response = await fetch(url, {
-        
-      });
+      const url = `https://api.rawg.io/api/games?page=${page}&page_size=50&key=47df0f4ea0b14eb3b3beaeba99f4e03d`;
+      const response = await fetch(url);
       const game = await response.json();
-      return game as Game[];
+      return game.results as Game[];
     } catch (error) {
       console.log();
     }
