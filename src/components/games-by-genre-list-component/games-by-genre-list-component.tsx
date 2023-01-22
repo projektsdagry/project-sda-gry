41a0a1077907653d10132d71212f17457d7268d4
Hubert Lewandowski <hubletto@gmail.com>
2023-01-22 17:30:44 +0100

    coś tam

--- src/components/games-by-genre-list-component/games-by-genre-list-component.tsx
+++ src/components/games-by-genre-list-component/games-by-genre-list-component.tsx
@@ -11,19 +11,22 @@ return <div className="cont">
             <table className="gameTable">
                 
                     <thead>
+                        <tr>
                         
                         <th>Name:</th>
                         <th>Released at:</th>
                         <th>Platforms:</th>
+                        </tr>
                     </thead>
                     
                     {gameList.map((gamesList) => (
                         <tbody key={gamesList.id}>
+                            <tr>
                         
-                    <td><img src={gamesList.background_image}></img> <p>{gamesList.name}</p> </td>
-                    <td> <p>{gamesList.released}</p></td>
+                    <td><img src={gamesList.background_image}></img> <p>{gamesList.name}</p>{""} </td>
+                    <td>{""} <p>{gamesList.released}</p></td>
                     <td>{gamesList.platforms.map((platforms) => platforms.platform.name ).join(",")}</td>
-                    
+                    </tr>
                     </tbody>    
                     ))}
                 
