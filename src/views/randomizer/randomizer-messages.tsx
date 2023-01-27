7180ba23d407ac663516dfa84bbb02482469e286
t1psyy <p.bankowski101@gmail.com>
2023-01-27 23:56:24 +0100

    feat: added randomizer messages

---
+++ src/views/randomizer/randomizer-messages.tsx
@@ -0,0 +1,33 @@
+const thanksMessages = [
+  "Glad you liked it 😊",
+  "Thanks for the love, we appreciate it ❤️",
+  "Glad you enjoyed it 🥳",
+  "Thanks for the input 💡",
+  "Your happiness is invaluable, thanks a ton 🙇‍♀️",
+  "We appreciate your feedback 💬",
+  "We appreciate your input, thanks for taking the time to share 🙏",
+  "You rock, thanks for the feedback 🤘",
+  "We are here for you 🥹",
+  "Glad we could help 😊",
+  "Thanks for being a part of our community, we appreciate you 🌟",
+  "Thanks for being part of our journey, we appreciate you 🚀",
+  "Thank you for your support 🙌",
+];
+const searchAgainMessages = [
+  "Unlocking new adventures 🗝️",
+  "Unveiling new games for your pleasure 🤩",
+  "Venturing into the galaxy for new games 🚀",
+  "Expanding your gaming options 🛍",
+  "Unleashing new gaming possibilities 🔥",
+  "Searching for something new to play 🔎",
+  "Finding new games for your enjoyment 🎉",
+  "Diving deeper into the gaming world 🌊",
+  "Broadening your gaming horizons 🌅",
+  "Helping you discover new worlds 🌍",
+  "Bringing you fresh games 🎲",
+  "Bringing you new games to conquer 🏆",
+  "Uncovering hidden gems 💎",
+  "Exploring new gaming experiences 🕹️",
+];
+
+export { thanksMessages, searchAgainMessages };
