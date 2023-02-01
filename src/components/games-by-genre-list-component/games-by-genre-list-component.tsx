5cd9552913d81f9cae66d1ce76b1e4dba7755e84
Hubert Lewandowski <hubletto@gmail.com>
2023-02-02 00:46:03 +0100

    feat: added icon navigate to /gamelist

--- src/components/games-by-genre-list-component/games-by-genre-list-component.tsx
+++ src/components/games-by-genre-list-component/games-by-genre-list-component.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { changeOrder, getGameListAsync, selectGamesByGenreList, selectOrder } from "../../slices/gamelist-slice";
+import {
+  changeOrder,
+  getGameListAsync,
+  selectGamesByGenreList,
+  selectOrder,
+} from "../../slices/gamelist-slice";
 import "./games-by-genre-list-component.css";
-import { useState,  } from "react";
-import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent  } from "@mui/material";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
+import UndoIcon from "@mui/icons-material/Undo";
+
 const ListOfGames: React.FC = () => {
   const [selectedPlatform, setSelectedPlatform] = useState("All");
-  const dispatch = useAppDispatch()
-  const order = useAppSelector(selectOrder)
-  const {gameId} = useParams();
- 
+  const dispatch = useAppDispatch();
+  const order = useAppSelector(selectOrder);
+  const { gameId } = useParams();
+  const navigate = useNavigate();
+
   let gameList = useAppSelector(selectGamesByGenreList);
 
-
-// Sort by platform
-  const handleChange = (event:SelectChangeEvent) => {
+  // Sort by platform
+  const handleChange = (event: SelectChangeEvent) => {
     setSelectedPlatform(event.target.value);
   };
 
@@ -26,55 +39,50 @@ const ListOfGames: React.FC = () => {
       )
     );
   }
-//Sort by else
 
-
-  const handleSortChange = async (e:SelectChangeEvent): Promise<void> => {
+  //Sort by realesed date, metacritic and popularity
+  const handleSortChange = async (e: SelectChangeEvent): Promise<void> => {
     dispatch(changeOrder(e.target.value));
-    if(gameId) {
-    dispatch(getGameListAsync({ gameId, order: e.target.value }));
+    if (gameId) {
+      dispatch(getGameListAsync({ gameId, order: e.target.value }));
     }
   };
-
-
-
 
   return (
     <div>
       <div className="sorting-section">
-       
-        <FormControl sx={{minWidth:'100px'}}>
+        <UndoIcon
+          sx={{
+            cursor: "pointer",
+          }}
+          onClick={() => navigate("/gamelist")}
+        >
+          {" "}
+        </UndoIcon>
+
+        <FormControl sx={{ minWidth: "100px" }}>
           <InputLabel id="demo-simple-select-label">Platform</InputLabel>
           <Select
-            
             value={selectedPlatform}
             label="Platform"
             onChange={handleChange}
           >
-            <MenuItem  value='All'>All</MenuItem>
-            <MenuItem value='PC'>PC</MenuItem>
-            <MenuItem value='macOS'>macOS</MenuItem>
-            <MenuItem value='Xbox Series S/X'>Xbox Series S/X</MenuItem>
-            <MenuItem value='PlayStation 5'>PlayStation 5</MenuItem>
-            <MenuItem value='Nintendo Switch'>Nintendo Switch</MenuItem>
+            <MenuItem value="All">All</MenuItem>
+            <MenuItem value="PC">PC</MenuItem>
+            <MenuItem value="macOS">macOS</MenuItem>
+            <MenuItem value="Xbox Series S/X">Xbox Series S/X</MenuItem>
+            <MenuItem value="PlayStation 5">PlayStation 5</MenuItem>
+            <MenuItem value="Nintendo Switch">Nintendo Switch</MenuItem>
           </Select>
         </FormControl>
-        <FormControl sx={{minWidth:'100px'}}>
+        <FormControl sx={{ minWidth: "100px" }}>
           <InputLabel id="sort">Order</InputLabel>
-          <Select
-            
-            value={order}
-            label="Order"
-            onChange={handleSortChange}
-          >
-            <MenuItem  value='popularity'>Popularity</MenuItem>
-            <MenuItem value='-metacritic'>Rating</MenuItem>
-            <MenuItem value='-released'>Released Date</MenuItem>
-           
-
+          <Select value={order} label="Order" onChange={handleSortChange}>
+            <MenuItem value="popularity">Popularity</MenuItem>
+            <MenuItem value="-metacritic">Rating</MenuItem>
+            <MenuItem value="-released">Released Date</MenuItem>
           </Select>
         </FormControl>
-
       </div>
       <div className="cont">
         <table className="gameTable">
@@ -88,8 +96,7 @@ const ListOfGames: React.FC = () => {
           </thead>
           <tbody>
             {gameList.map((gamesList) => (
-            
-              <tr className='table-row' key={gamesList.id}>
+              <tr className="table-row" key={gamesList.id}>
                 <td>
                   <img alt="background_img" src={gamesList.background_image} />{" "}
                   <p>{gamesList.name}</p>
@@ -106,7 +113,6 @@ const ListOfGames: React.FC = () => {
                     .join(",")}
                 </td>
               </tr>
-            
             ))}
           </tbody>
         </table>
