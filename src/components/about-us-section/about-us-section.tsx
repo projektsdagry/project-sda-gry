7f819692da93efa04c05b8f32e612851ae741085
Hubert Lewandowski <hubletto@gmail.com>
2023-02-20 18:12:24 +0100

    feat:refactor homepage

--- src/components/about-us-section/about-us-section.tsx
+++ src/components/about-us-section/about-us-section.tsx
@@ -1,12 +1,13 @@
 import { Grid, Typography } from "@mui/material";
 import sheep from "../../assets/sheep.png";
+import { StyledImg, StyledSection } from "../../styled/about-us/about-us";
 import hubs from "./../../assets/hubs.png";
 import raccoon from "./../../assets/raccoon.png";
 import "./about-us-section.css";
 
 export const AboutUs = () => {
   return (
-    <section className="container" id="sectionUs">
+    <StyledSection id="sectionUs">
       <Grid
         container
         justifyContent="center"
@@ -21,20 +22,20 @@ export const AboutUs = () => {
 
         <Grid className="img-container" item xs={12} sm={4}>
           <a href="https://github.com/Owieczek" target={"_blank"}>
-            <img title="sheep" src={sheep} />
+            <StyledImg title="sheep" src={sheep} />
           </a>
         </Grid>
         <Grid className="img-container" item xs={12} sm={4}>
           <a href="https://github.com/t1psyy" target={"_blank"}>
-            <img title="raccoon" src={raccoon} />
+            <StyledImg title="raccoon" src={raccoon} />
           </a>
         </Grid>
         <Grid className="img-container" item xs={12} sm={4}>
           <a href="https://github.com/lewandowskihubert" target={"_blank"}>
-            <img title="hubs" src={hubs} />
+            <StyledImg title="hubs" src={hubs} />
           </a>
         </Grid>
       </Grid>
-    </section>
+    </StyledSection>
   );
 };
---
+++ src/styled/about-us/about-us.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled"
+
+export const StyledSection = styled.section `
+    min-height: 100vh;
+    text-align: center;
+
+`
+export const StyledImg = styled.img `
+    border-radius: 50%;
+    width: 300px;
+    height:300px;
+`
+
